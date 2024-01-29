d6f2fed209ed3cbec4e75465c21e9c8bfbae3ade
mestrinio <ebis.pedromartins@gmail.com>
2024-01-29 19:59:37 +0000

    cube detection working properly

--- robutler_bringup_tp3/scripts/tts.py
+++ robutler_bringup_tp3/scripts/tts.py
@@ -4,7 +4,7 @@ import gtts
 def text_to_speech(text, language='en', speed='normal'):
 
   # Create a gTTS object.
-  tts = gtts.gTTS(text, lang=language, slow=False if speed == 'normal' else True)
+  tts = gtts.gTTS(text, lang=language, slow=False if speed == 'normal' else True, tld='ca')
 
   # Save the audio file.
   audio_file = '/home/mestre/catkin_ws/src/TP3/PSR_Trabalho3/robutler_bringup_tp3/scripts/Audio/' + text + '.mp3'
